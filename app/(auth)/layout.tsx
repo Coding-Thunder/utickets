60218a6f7b5024662f3ba7a6ff7ef832b761660f
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2024-08-07 15:52:45 +0530

    ADD: CSS Meta

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,32 +1,17 @@
-import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google"
-import "./globals.css";
-import SupportHeader from "@/components/Home/SupportHeader";
-import Header from "@/components/Commmon/Header";
-import Footer from "@/components/Commmon/Footer";
-import { cn } from "@/lib/utils";
-import Disclaimer from "@/components/Commmon/Disclaimer";
-
-const fontSans = FontSans({
-    subsets: ["latin"],
-    variable: "--font-sans",
-})
+import { Metadata } from 'next';
+import React, { ReactNode } from 'react'
 
 export const metadata: Metadata = {
-    title: "Registes Ticketss",
-    description: "Book cheap flight tickets",
+    title: "Register",
+    description: "Login and Signup",
 };
 
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
+const AuthLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <html lang="en">
-            <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
-                {children}
-            </body>
-        </html>
-    );
+        <main>
+            {children}
+        </main>
+    )
 }
+
+export default AuthLayout
