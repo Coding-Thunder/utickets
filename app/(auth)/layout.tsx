6ddeea9d98dea1ba72b902699c61d8b4f83d6b6f
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2024-08-07 15:30:21 +0530

    ADD: CSS Meta

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google"
+import "./globals.css";
+import SupportHeader from "@/components/Home/SupportHeader";
+import Header from "@/components/Commmon/Header";
+import Footer from "@/components/Commmon/Footer";
+import { cn } from "@/lib/utils";
+import Disclaimer from "@/components/Commmon/Disclaimer";
+
+const fontSans = FontSans({
+    subsets: ["latin"],
+    variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+    title: "Universal Ticketss",
+    description: "Book cheap flight tickets",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+                {children}
+            </body>
+        </html>
+    );
+}
