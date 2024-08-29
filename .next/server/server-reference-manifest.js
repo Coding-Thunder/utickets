731a202c96ffb08fa715fb5b838acb7d1910399f
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2024-08-29 13:01:53 +0530

    ADD: new build release

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"MKQKYDuXP0o7A2U5Kwd1KL8CTXWucRnK8EIcRhcGIXQ=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"jSuGK25Mnw+7xwZ2GACZa/cafK78loc/5Lh92Nb0zIk=\"}"
